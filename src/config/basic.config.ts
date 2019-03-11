1375331799da091c00aad751f425614727544c9d
medivh <medivh@jollycorp.com>
2019-03-11 22:29:13 +0800

    update

--- src/config/basic.config.ts
+++ src/config/basic.config.ts
@@ -3,33 +3,36 @@ import * as fs   from 'fs';
 
 let privateKeyPath: string;
 let publicKeyPath: string;
+let attachedFilesPath: string;
 
-switch(process.env.APP_ENV){
-    case 'development': 
-        privateKeyPath = path.join(__dirname, '../../keys/note_key');
-        publicKeyPath = path.join(__dirname, '../../keys/note_key.pub');
-    break;
-    case 'production': 
-        privateKeyPath = path.join(__dirname, '../../../keys/note_key');
-        publicKeyPath = path.join(__dirname, '../../../keys/note_key.pub');
-    break;
+switch (process.env.APP_ENV) {
+    case 'development':
+        privateKeyPath    = path.join(__dirname, '../../keys/note_key');
+        publicKeyPath     = path.join(__dirname, '../../keys/note_key.pub');
+        attachedFilesPath = path.join(__dirname, '../../attached_files');
+        break;
+    case 'production':
+        privateKeyPath    = path.join(__dirname, '../../../keys/note_key');
+        publicKeyPath     = path.join(__dirname, '../../../keys/note_key.pub');
+        attachedFilesPath = path.join(__dirname, '../../../attached_files');
+        break;
 }
 
 const privateKey = fs.readFileSync(privateKeyPath);
-const publicKey = fs.readFileSync(publicKeyPath);
+const publicKey  = fs.readFileSync(publicKeyPath);
 
 export default {
     PATH: {
-        BASIC   : path.join(__dirname, '../../'),
-        ATTACHED_FILES: path.join(__dirname, '../../attached_files')
+        BASIC         : path.join(__dirname, '../../'),
+        ATTACHED_FILES: attachedFilesPath
     },
     KEYS: {
         PRIVATE: privateKey,
-        PUBLIC :publicKey
+        PUBLIC : publicKey
     },
-    ASE: {
-      KEY: '9vApxLk5G3PAsJrM',
-      IV: 'FnJL7EDzjqWjcaY9'
+    ASE : {
+        KEY: '9vApxLk5G3PAsJrM',
+        IV : 'FnJL7EDzjqWjcaY9'
     },
     DB  : {
         DB_TYPE    : process.env.DB_TYPE,
