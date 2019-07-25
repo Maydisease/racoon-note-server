f4004684b3454ce31f323cf55697f1aa19c3b108
tandongsheng <medivh@jollycorp.com>
2019-07-25 15:37:43 +0800

    [update] 1. 增加 dart、yaml 语法的高亮展示

--- src/modules/frontend/note/article/article.controller.ts
+++ src/modules/frontend/note/article/article.controller.ts
@@ -19,6 +19,8 @@ import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-sass';
 import 'prismjs/components/prism-scss';
 import 'prismjs/components/prism-textile';
+import 'prismjs/components/prism-dart';
+import 'prismjs/components/prism-yaml';
 
 const markdownItMermaid = require('markdown-it-mermaid').default;
 const markdownItImsize  = require('markdown-it-imsize');
